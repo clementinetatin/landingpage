eae5b77c26fc54bd174821e84a9f128fb2fd0e57
clementinetatin <64962470+clementinetatin@users.noreply.github.com>
2020-05-28 11:42:57 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -13,10 +13,11 @@
  *
 */
 
-/**
- * Define Global Variables
- *
-*/
+ //Define Global Variables
+
+const sections = Array.from(document.querySelectorAll('section[data-nav]'));
+const ul = document.querySelectorAll('ul');
+const nav_list = document.getElementsByClassName("navbar__list");
 
 
 /**
@@ -34,58 +35,18 @@
 */
 
 // build the nav
-const sections = Array.from(document.querySelectorAll('section[data-nav]'));
-const ul = document.querySelectorAll('ul');
-const sectionId = sections.id;
 
 for (let i=0; i<sections.length;i++) {
   const li = document.createElement('li');
   const anch = document.createElement('a');
-  a.innerText = sections[i].id;
-  var addAnchToLi = li.appendChild(a);
-  document.getElementById("navbar__list").appendChild(addAnchToLi);
-  anch.setAttribute("href", '#${sectionId}');
+  const sectionId = sections[i].id;
+  anch.innerHTML = sections[i].id;
+  anch.setAttribute("href", `#${sectionId}`);
+  nav_list[0].appendChild(li);
+  li.appendChild(anch);
 };
 
-
-/*function createLi() {
-  sections.forEach((section)=> {
-    li.innerText = section;
-    ul.appendChild(li);
-  });
-};
-createLi();
-
-function createAnchor() {
-  sections.forEach((section) => {
-    a.innerText = section;
-    ul.appendChild(a);
-  });
-};
-createAnchor();*/
-
-
-
-/*function createMenu() {
-  links.forEach((link)=> {
-    const li = document.createElement('li');
-    li.innerText = link;
-    ul.appendChild(li);
-  })
-}
-
-createMenu();*/
-
-
-/*links.forEach((link)=> {
-  const li = document.createElement('li');
-  const li = document.createElement('a');
-  li.innerText = link;
-  ul.appendChild(li);
-})*/
-
-
-
+//burger and menu in mobile format
 const navSlide = () => {
   const burger = document.querySelector('.burger');
   const nav = document.querySelector('.navbar__list');
@@ -104,7 +65,63 @@ navSlide();
 
 
 // Add class 'active' to section when near top of viewport
+/*function makeActive() {
+  for (const section of sections) {
+    const bounding = section.getBoundingClientRect();
+    return (
+        bounding.top <= 150 &&
+        bounding.bottom >= 150;
+    );
+  }
+}*/
 
+function isInViewport (element) {
+  const bounding = element.getBoundingClientRect();
+  if (bounding.top >=0 &&
+    bounding.left >= 0 &&
+    bounding.right <= (window.innerWidth || document.documentElement.clientWidth)&&
+    bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+function ifTrue() {
+  if(isInViewport() === true) {
+    sections.classList.add('your-active-class');
+  } else {
+    sections.classList.remove('your-active-class');
+  }
+}
+
+isInViewport();
+ifTrue();
+
+/*document.addEventListener("scroll", function() {
+  makeActive();
+);
+};*/
+
+
+/*const bounding = sections.getBoundingClientRect();
+if (
+  bounding.top >=0 &&
+  bounding.left >= 0 &&
+  bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+  bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+) {
+  console.log('in the viewport !');
+} else {
+  console.log('not in the viewport');
+};
+
+
+
+// call function to make it nav_active
+
+})
 
 // Scroll to anchor ID using scrollTO event
 
